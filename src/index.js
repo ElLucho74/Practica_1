4578775df41e47eb595ebc1d863d02d2632dcb7e
Luis Antonio Castro Iturbide <118926456+ElLucho74@users.noreply.github.com>
2023-01-13 14:37:24 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi Propio Code Sandbox!</h1>
+<h2> Luis Castro Iturbide </h2>
+
+<div>
+ Universidad Tecnologica de Durango
+  <a href="https://github.com/ElLucho74/contador.git" target="_blank" rel="noopener noreferrer">Mi github</a>.
+</div>
+`;
